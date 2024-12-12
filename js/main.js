e96c391b1a3605e0fba866ecd2193a3dda94e31b
manarelabsi01 <manarelabsi395@gmail.com>
2024-12-12 14:12:52 +0200

    animation

---
+++ js/main.js
@@ -0,0 +1,7 @@
+//!-------- typed js libarary --------!//
+var typed = new Typed('#element', {
+    strings: [ 'Designer' , 'Developer' , 'Freelancer' , 'Photographer'],
+    typeSpeed: 40,
+    backSpeed: 40,
+    loop: true
+  });
